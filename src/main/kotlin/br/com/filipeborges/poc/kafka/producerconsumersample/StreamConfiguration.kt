6fb12f4f90d61e905188a27b6ed2f29f84f1466e
Filipe Borges <filipebkc2209@gmail.com>
2024-02-18 17:50:50 -0300

    Reactive sample implemented

--- src/main/kotlin/br/com/filipeborges/poc/kafka/producerconsumersample/StreamConfiguration.kt
+++ src/main/kotlin/br/com/filipeborges/poc/kafka/producerconsumersample/StreamConfiguration.kt
@@ -2,6 +2,8 @@ package br.com.filipeborges.poc.kafka.producerconsumersample
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Configuration
 class StreamConfiguration {
@@ -11,6 +13,14 @@ class StreamConfiguration {
             println("======> Received text: $it")
             if (it == "error") throw RuntimeException("runtime error")
             it.uppercase()
+        }
+    }
+
+    @Bean
+    fun uppercaseReactive(): java.util.function.Function<Flux<String>, Flux<String>> {
+        return java.util.function.Function { message ->
+            message.doOnNext { println("======> Received text: $it") }
+                    .map { if (it != "error") it.uppercase() else throw RuntimeException("runtime error") }
         }
     }
 }
--- src/test/kotlin/br/com/filipeborges/poc/kafka/producerconsumersample/ProducerConsumerSampleApplicationTests.kt
+++ src/test/kotlin/br/com/filipeborges/poc/kafka/producerconsumersample/ProducerConsumerSampleApplicationTests.kt
@@ -1,13 +1,13 @@
 package br.com.filipeborges.poc.kafka.producerconsumersample
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.stream.binder.test.InputDestination
 import org.springframework.cloud.stream.binder.test.OutputDestination
 import org.springframework.messaging.support.GenericMessage
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest
 class ProducerConsumerSampleApplicationTests {
 
@@ -35,13 +35,66 @@ class ProducerConsumerSampleApplicationTests {
 
 	@Test
 	fun notReactiveTransformationError() {
-		val myText = "error"
-		val event = GenericMessage(myText)
+		// Force error on first message
+		var myText = "error"
+		var event = GenericMessage(myText)
 
 		inputDestination.send(event, "upper-not-reactive-input")
-		val received = outputDestination.receive(5000L, "upper-not-reactive-output")
+		var received = outputDestination.receive(5000L, "upper-not-reactive-output")
+
+		Assertions.assertNull(received)
+
+		// Check that second message will be consumed be imperative consumer
+		myText = "my-text"
+		event = GenericMessage(myText)
+
+		inputDestination.send(event, "upper-not-reactive-input")
+		received = outputDestination.receive(2000L, "upper-not-reactive-output")
+
+		Assertions.assertNotNull(received)
+
+		var transformedText = String(received.payload)
+
+		Assertions.assertNotNull(transformedText)
+		Assertions.assertEquals("MY-TEXT", transformedText)
+	}
+
+	@Order(1)
+	@Test
+	fun reactiveTransformation() {
+		val myText = "my-text"
+		val event = GenericMessage(myText)
+
+		inputDestination.send(event, "upper-reactive-input")
+		val received = outputDestination.receive(2000L, "upper-reactive-output")
+
+		Assertions.assertNotNull(received)
+
+		val transformedText = String(received.payload)
+
+		Assertions.assertNotNull(transformedText)
+		Assertions.assertEquals("MY-TEXT", transformedText)
+	}
+
+	@Order(2)
+	@Test
+	fun reactiveTransformationError() {
+		// Force error on first message
+		var myText = "error"
+		var event = GenericMessage(myText)
+
+		inputDestination.send(event, "upper-reactive-input")
+		var received = outputDestination.receive(5000L, "upper-reactive-output")
+
+		Assertions.assertNull(received)
+
+		// On reactive model, the Flux pipeline stops to receive messages - error signal end the Flux (reactive streams spec)
+		myText = "my-text-2"
+		event = GenericMessage(myText)
+
+		inputDestination.send(event, "upper-reactive-input")
+		received = outputDestination.receive(2000L, "upper-reactive-output")
 
 		Assertions.assertNull(received)
 	}
-
 }
